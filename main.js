9a39bf565bf7d4d5f89b70fbf9bcd2351df78bde
PatricioDVG <140539670+PatricioDVG@users.noreply.github.com>
2024-05-27 16:35:54 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+noseX=0;
+noseY=0;
+function preload() {
+    shrek=loadImage('https://i.postimg.cc/P5fYY2kz/shreksito-buenito.png');
+}
+
+function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+
+}
+function modelLoaded(){
+    console.log('PoseNet está inicializado')
+}
+
+function gotPoses(results)
+{
+    if(results.length >0)
+    {
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("nose x = " + results[0].pose.nose.x);
+        console.log("nose y = " + results[0].pose.nose.y);
+
+    }
+}
+
+function draw() {
+    image(video, 0, 0, 300, 300);
+    image(shrek,noseX-50, noseY-90, 120, 200);
+}
+function take_snapshot(){
+    save('myFilterImage.png');
+}
